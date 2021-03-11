a476d5e2aa8a183e9a1ea5b67633c0546747d920
Pavel <nomerdvadcatpyat@gmail.com>
2021-03-11 20:06:21 +0500

    fix full image layout position

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ dotenv.config({ path: root('.env') });
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/test',
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/gallery',
   SESSION_SECRET: process.env.SESSION_SECRET || 'some-string',
   FULL_IMAGES_DESTINATION: process.env.FULL_IMAGES_DESTINATION || 'images/full',
   MIN_IMAGES_DESTINATION: process.env.MIN_IMAGES_DESTINATION || 'images/resized',
